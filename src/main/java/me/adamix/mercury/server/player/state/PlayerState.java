9ab6653650a5325b4839664001503b26497ea141
AdamIx <adamburdik9@gmail.com>
2024-12-23 19:07:11 +0100

    Update PlayerState.java

--- src/main/java/me/adamix/mercury/server/player/state/PlayerState.java
+++ src/main/java/me/adamix/mercury/server/player/state/PlayerState.java
@@ -4,4 +4,8 @@ public enum PlayerState {
 	INIT, // Player just joined server ( data from database is not loaded yet )
 	LIMBO, // Player data is loaded from database. In this state player is choosing his profile
 	PLAY; // All data is loaded and player chose his profile.
+
+	public boolean isPlayable() {
+		return this != INIT && this != LIMBO;
+	}
 }

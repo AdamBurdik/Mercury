7e124581cd4a72612bcbc1ac07751c6649a6e05e
AdamIx <adamburdik9@gmail.com>
2024-12-25 17:21:58 +0100

    Change: item description array to list

--- src/main/java/me/adamix/mercury/server/item/component/ItemDescriptionComponent.java
+++ src/main/java/me/adamix/mercury/server/item/component/ItemDescriptionComponent.java
@@ -2,6 +2,8 @@ package me.adamix.mercury.server.item.component;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,14 +23,20 @@ public record ItemDescriptionComponent(@NotNull String[] lines) implements Mercu
 
 	@Override
 	public Map<String, Object> serialize() {
-		return Map.of("lines", this.lines);
+		return Map.of("lines", List.of(this.lines));
 	}
 
 	public static @NotNull ItemDescriptionComponent deserialize(Map<String, Object> map) {
 		Object linesObject = map.get("lines");
 
-		if (linesObject instanceof String[] linesArray) {
-			return new ItemDescriptionComponent(linesArray);
+		if (linesObject instanceof List<?> objectList) {
+			List<String> lines = new ArrayList<>();
+			for (Object object : objectList) {
+				if (object instanceof String string) {
+					lines.add(string);
+				}
+			}
+			return new ItemDescriptionComponent(lines.toArray(new String[]{}));
 		} else {
 			throw new IllegalArgumentException("Invalid type for 'lines' in the map");
 		}

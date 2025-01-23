76e84b482d4ac6464071845cbb0af2cdcb4ed622
AdamIx <adamburdik9@gmail.com>
2025-01-23 20:25:19 +0100

    Fix: set player status after sending him to limbo

--- src/main/java/me/adamix/mercury/server/command/ClassCommand.java
+++ src/main/java/me/adamix/mercury/server/command/ClassCommand.java
@@ -3,6 +3,7 @@ package me.adamix.mercury.server.command;
 import me.adamix.mercury.server.Server;
 import me.adamix.mercury.server.inventory.ProfileSelectionInventory;
 import me.adamix.mercury.server.player.MercuryPlayer;
+import me.adamix.mercury.server.player.state.PlayerState;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.potion.Potion;
@@ -21,6 +22,7 @@ public class ClassCommand extends Command {
 				player.setGameMode(GameMode.CREATIVE);
 				player.addEffect(new Potion(PotionEffect.BLINDNESS, Byte.MAX_VALUE, Potion.INFINITE_DURATION));
 				player.sendToLimbo();
+				player.setState(PlayerState.LIMBO);
 
 				Server.getProfileDataManager().getProfileDataListSync(player.getUuid(), (playerDataList -> {
 					ProfileSelectionInventory inventory = new ProfileSelectionInventory(playerDataList);
